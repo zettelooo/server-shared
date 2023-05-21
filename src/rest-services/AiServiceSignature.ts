3519184beea1381ed1599d6ab3dc05c40bc56136
Hessamoddin A Shokravi <ahs502@gmail.com>
2023-05-21 18:12:06 +0330

    AI services Smart command end-point

--- src/rest-services/AiServiceSignature.ts
+++ src/rest-services/AiServiceSignature.ts
@@ -1,3 +1,5 @@
+import { ReadonlyRecord } from '@zettelooo/commons'
+
 export namespace AiServiceSignature {
   export namespace OpenAiChatGpt {
     export interface Request {
@@ -13,6 +15,20 @@ export namespace AiServiceSignature {
     }
     export interface Response {
       readonly answer: string
+    }
+  }
+
+  export namespace SmartPageCommand {
+    export interface Request {
+      readonly api: ReadonlyRecord<string, Action>
+      readonly command: string
+    }
+    export interface Response {
+      readonly code: string | null
+    }
+    export interface Action {
+      readonly signatures: readonly string[]
+      readonly extendSignaturesWith?: 'available extensions'
     }
   }
 }
