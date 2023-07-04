2a126e37e285933cb57e1d722b874fab840e6b00
Hessamoddin A Shokravi <ahs502@gmail.com>
2023-07-04 15:51:13 +0330

    Fix Account model type

--- src/Model/models/Account.ts
+++ src/Model/models/Account.ts
@@ -1,7 +1,9 @@
 import { Base } from '../Base'
+import { Type } from '../Type'
 import { User } from './User'
 
 export interface Account extends Omit<User, 'type'>, Base {
+  readonly type: Type.Account
   readonly isOnboarded: boolean
   readonly walletAddress: string | null
 }
