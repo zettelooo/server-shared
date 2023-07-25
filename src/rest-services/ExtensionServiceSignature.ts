29ad0f18a3e7d3e53a9bb4f0a323319a24a3e0b5
Hessamoddin A Shokravi <ahs502@gmail.com>
2023-07-25 17:13:10 +0330

    Minor modifications

--- src/rest-services/ExtensionServiceSignature.ts
+++ src/rest-services/ExtensionServiceSignature.ts
@@ -27,30 +27,19 @@ export namespace ExtensionServiceSignature {
   export namespace ModifyConfiguration {
     export interface Request {
       readonly intention: string
-      readonly extensionConfiguration: Model.ExtensionConfiguration
+      readonly configuration: Model.ExtensionConfiguration
     }
     export interface Response {
-      readonly extensionConfiguration: Model.ExtensionConfiguration
+      readonly configuration: Model.ExtensionConfiguration
     }
   }
 
   export namespace RefineConfiguration {
     export interface Request {
-      readonly extensionConfiguration: Model.ExtensionConfiguration
+      readonly configuration: Model.ExtensionConfiguration
     }
     export interface Response {
-      readonly extensionConfiguration: Model.ExtensionConfiguration
-    }
-  }
-
-  export namespace UpdateCardPublicData {
-    export interface Request {
-      readonly cardId: Id
-      readonly extensionId: Id
-      readonly cardPublicData: unknown
-    }
-    export interface Response {
-      readonly cardPublicDataDictionary: Model.DataDictionary
+      readonly configuration: Model.ExtensionConfiguration
     }
   }
 }
